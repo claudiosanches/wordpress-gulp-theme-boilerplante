cd1895046694e7857fe49f45445d75a83fffb7b7
claudiosmweb <contato@claudiosmweb.com>
2014-01-07 22:16:00 -0200

    sets assets directories in a variable

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -1,49 +1,60 @@
 /* jshint node:true */
+
+// Include project requirements.
 var gulp = require( 'gulp' ),
 	jshint = require( 'gulp-jshint' ),
 	uglify = require( 'gulp-uglify' ),
 	sass = require( 'gulp-sass' ),
 	imagemin = require('gulp-imagemin');
 
+// Sets assets folders.
+var dirs = {
+	js: '../assets/js',
+	css: '../assets/css',
+	sass: '../assets/sass',
+	images: '../assets/images',
+	fonts: '../assets/fonts'
+};
+
 gulp.task( 'scripts', function () {
 	// Hint all JavaScript.
-	gulp.src( '../assets/js/client/*.js' )
+	gulp.src( dirs.js + '/client/*.js' )
 		.pipe( jshint() )
 		.pipe( jshint.reporter( 'default' ) );
 
 	// Minify and copy all JavaScript.
-	gulp.src( '../assets/js/client/*.js' )
+	gulp.src( dirs.js + '/client/*.js' )
 		.pipe( uglify() )
-		.pipe( gulp.dest( '../assets/js/build' ) );
+		.pipe( gulp.dest( dirs.js + '/build' ) );
 });
 
 gulp.task( 'sass', function () {
 	// Compile all SCSS files.
-	gulp.src( '../assets/sass/*.scss' )
+	gulp.src( dirs.sass + '/*.scss' )
 		.pipe( sass({
 			outputStyle: 'compressed'
 		}) )
-		.pipe( gulp.dest( '../assets/css' ) );
+		.pipe( gulp.dest( dirs.css ) );
 });
 
 gulp.task( 'optimize', function () {
 	// Optimize all images.
-	gulp.src( '../assets/images/*.{png,jpg,gif}' )
+	gulp.src( dirs.images + '/*.{png,jpg,gif}' )
 		.pipe( imagemin({
 			optimizationLevel: 7,
 			progressive: true
 		}) )
-		.pipe( gulp.dest( '../assets/images/' ) );
+		.pipe( gulp.dest( dirs.images ) );
 });
 
 gulp.task( 'watch', function () {
 	// Watch JavaScript changes.
-	gulp.watch( '../assets/js/client/*.js', function () {
+	gulp.watch( dirs.js + '/client/*.js', function () {
 		gulp.run( 'scripts' );
 	});
 
 	// Watch SCSS changes.
-	gulp.watch( '../assets/sass/*.scss', function () {
+	gulp.watch( dirs.sass + '/*.scss', function () {
 		gulp.run( 'sass' );
 	});
 });
